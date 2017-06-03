86a1edd4ba0956a54237f269db2646817f1854d6
Xander Deng <dengxiang2010@gmail.com>
2017-06-03 13:01:58 +0800

    Initial Commit

---
+++ TouchDock/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TouchDock
+//
+//  Created by 邓翔 on 2017/6/3.
+//  Copyright © 2017年 ddddxxx. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
