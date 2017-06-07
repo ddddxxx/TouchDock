6aeb32c4f0d72bdbb75b26fa15a42078de62f8f8
Xander Deng <dengxiang2010@gmail.com>
2017-06-07 11:33:44 +0800

    expose more undocumented api

--- TouchDock/CBridge/TouchBar.h
+++ TouchDock/CBridge/TouchBar.h
@@ -21,22 +21,27 @@
 #import <AppKit/AppKit.h>
 
 extern void DFRElementSetControlStripPresenceForIdentifier(NSTouchBarItemIdentifier, BOOL);
+
 extern void DFRSystemModalShowsCloseBoxWhenFrontMost(BOOL);
 
 @interface NSTouchBarItem ()
 
 + (void)addSystemTrayItem:(NSTouchBarItem *)item;
+
 + (void)removeSystemTrayItem:(NSTouchBarItem *)item;
 
 @end
 
 @interface NSTouchBar ()
 
+// presentSystemModalFunctionBar:placement:systemTrayItemIdentifier:
+// v40@0:8@16q24@32
++ (void)presentSystemModalFunctionBar:(NSTouchBar *)touchBar placement:(long long)placement systemTrayItemIdentifier:(NSTouchBarItemIdentifier)identifier;
+
 + (void)presentSystemModalFunctionBar:(NSTouchBar *)touchBar systemTrayItemIdentifier:(NSTouchBarItemIdentifier)identifier;
+
++ (void)dismissSystemModalFunctionBar:(NSTouchBar *)touchBar;
+
 + (void)minimizeSystemModalFunctionBar:(NSTouchBar *)touchBar;
 
 @end
-
-extern CFArrayRef _LSCopyApplicationArrayInFrontToBackOrder(uint32_t sessionID);
-extern void _LSASNExtractHighAndLowParts(void const* asn, UInt32* psnHigh, UInt32* psnLow);
-extern CFTypeID _LSASNGetTypeID(void);
